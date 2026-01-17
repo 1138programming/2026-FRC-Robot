d8219daf775a1d77581fc97f7a53904dd920f75c
BrightonGannaway <begannaway@icloud.com>
2026-01-16 20:26:57 -0800

    Limelight documentation and getters

--- 2026-FRC-Robot/src/main/java/frc/robot/subsystems/Limelight.java
+++ 2026-FRC-Robot/src/main/java/frc/robot/subsystems/Limelight.java
@@ -17,21 +17,34 @@ public class Limelight extends SubsystemBase {
     private Pose2d poseEstimate = new Pose2d();
     private final Field2d field = new Field2d();
 
-    private double botPoseX;
-    private double botPoseY;
-    private double botPoseZ;
-    private double roll;
-    private double pitch;
-    private double yaw;
-    private double latency;
-    private double numberOfTargetsSeen;
-    private double tagSpan;
-    private double averageDistance;
-    private double Area;
-    private double tableOne;
-    private double target;
-    private double ID;
+    //Robot position in fieldspace from AprilTag pipeline
+    private double botPoseX; //bot translation in X
+    private double botPoseY; //bot translation in Y
+    private double botPoseZ; //bot translation in Z
 
+    //Robot space camera position
+    private double roll; //bot rotation around X
+    private double pitch; //bot rotation around Y
+    private double yaw; //bot rotation around Z
+
+    //Limelight Camera view data
+    //resolves to target tix (most prevalent tag)
+    private double tx; //horizontal offset
+    private double ty; //vertical offset
+
+    //View of tag data
+    private double numberOfTargetsSeen; //number of targets seen
+    private double tagSpan; //span between detected tags
+    private double averageDistance; //average distance to detected tags
+    private double Area; //average area of detected tags
+
+    private double ID; //tag id in primary view
+    private int priorityID; //highest priority tag id in view - ignore all other april tags
+
+    //other 
+    private double latency;
+    private int isTargetsDetected;
+  
 
    
 
@@ -40,8 +53,6 @@ public class Limelight extends SubsystemBase {
     LimelightOneTable = NetworkTableInstance.getDefault().getTable("limelight");
 
     botpose = LimelightOneTable.getEntry("botpose_wpiblue").getDoubleArray(new double[11]);
-
-    tableOne = LimelightOneTable.getEntry("tableOne").getDouble(0);
 
     if (botpose.length != 0) {
       botPoseX = botpose[0];
@@ -58,20 +69,117 @@ public class Limelight extends SubsystemBase {
       ID = botpose[11];
 
     }
+
+    priorityID = -1;
+    //LimelightOneTable.getEntry("priorityid").setNumber(priorityID);
+
   //stores the data for the LL
-  
   }
+
   public void periodic() {
     //Gets whatever the vaaibale names are
-    target = LimelightOneTable.getEntry("tv").getDouble(0);
-   botPoseX = LimelightOneTable.getEntry("TX").getDouble(0);  
-   botPoseY = LimelightOneTable.getEntry("TY").getDouble(0);  
-   Area = LimelightOneTable.getEntry("TA").getDouble(0); 
-   ID = LimelightOneTable.getEntry("TID").getDouble(0);  
-   LimelightOneTable.getEntry("TID").getDoubleArray(new double[41]);
+    isTargetsDetected = (int)LimelightOneTable.getEntry("tv").getInteger(0);
+    tx = LimelightOneTable.getEntry("tx").getDouble(0);  
+    ty = LimelightOneTable.getEntry("ty").getDouble(0);  
+    Area = LimelightOneTable.getEntry("ta").getDouble(0); 
+    ID = LimelightOneTable.getEntry("tid").getDouble(0);  
 
+    if (botpose.length != 0) {
+      botPoseX = botpose[0];
+      botPoseY = botpose[1];
+      botPoseZ = botpose[2];
+      roll = botpose[3];
+      pitch = botpose[4];
+      yaw = botpose[5];
+      latency = botpose[6];
+      numberOfTargetsSeen = botpose[7];
+      tagSpan = botpose[8];
+      averageDistance = botpose[9];
+      Area = botpose[10];
+      ID = botpose[11];
 
-}
+    }
+  }
+
+  //botpose getters
+
+  public double getBotPoseX() {
+    return botPoseX;
+  }
+
+  public double getBotPoseY() {
+    return botPoseY;
+  }
+
+  public double getBotPoseZ() {
+    return botPoseZ;
+  }
+
+  public double getRoll() {
+    return roll;
+  }
+
+  public double getPitch() {
+    return pitch;
+  }
+
+  public double getYaw() {
+    return yaw;
+  }
+
+  public double getLatency() {
+    return latency;
+  }
+
+  public double getNumberOfTargetsSeen() {
+    return numberOfTargetsSeen;
+  }
+
+  public double getTagSpan() {
+    return tagSpan;
+  }
+
+  public double getAverageDistance() {
+    return averageDistance;
+  }
+
+  public double getArea() {
+    return Area;
+  }
+
+  public double getID() {
+    return ID;
+  }
+
+  public void setPriorityID(int id) {
+    priorityID = id;
+    LimelightOneTable.getEntry("priorityid").setNumber(priorityID);
+  }
+
+  //figure out if this works or not. Is priority id tracked after all tags are processed. 
+  public void resetPriorityID() {
+    priorityID = -1;
+    LimelightOneTable.getEntry("priorityid").setNumber(priorityID);
+  }
+
+  public int getPriorityID() {
+    return priorityID;
+  }
+
+  //everything else
+
+  public double getTx() {
+    return tx;
+  }
+
+  public double getTy() {
+    return ty;
+  }
+
+  public int getIsTargetsDetected() {
+    return isTargetsDetected;
+  }
+
 
 }
   
